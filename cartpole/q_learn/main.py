1ddc86a1cbfd92d88ba6843f2ddf494208c53b70
Lukas <lukasz.ksiezak@interia.pl>
2020-11-01 19:24:41 +0100

    cartpole solution using q learning

--- cartpole/q_learn/main.py
+++ cartpole/q_learn/main.py
@@ -43,8 +43,8 @@ END_EPSILON_DECAYING = EPISODES // 2
 
 epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
-# q_table = np.random.uniform(
-#     low=-2, high=0, size=(DISCRETE_OBSERVATION_SPACE_SIZE + [env.action_space.n]))
+q_table = np.random.uniform(
+    low=-2, high=0, size=(DISCRETE_OBSERVATION_SPACE_SIZE + [env.action_space.n]))
 
 ep_rewards = []
 
@@ -89,3 +89,75 @@ env.close()
 
 # As a min and max, experimental values from above will
 # be multiplied by 100 for algorithm
+
+
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low) / discrete_observation_space_window_size
+    return tuple(discrete_state.astype(np.int))
+
+print("Started learning")
+for episode in range(EPISODES):
+    episode_reward = 0
+
+    discrete_state = get_discrete_state(env.reset())
+    done = False
+    if episode % SHOW_EVERY == 0:
+        print(episode)
+        render = True
+    else:
+        render = False
+
+    while not done:
+
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[discrete_state])
+        else:
+            action = np.random.randint(0, env.action_space.n)
+
+        new_state, reward, done, _ = env.step(action)
+        episode_reward += reward
+        new_discrete_state = get_discrete_state(new_state)
+
+        if render:
+            env.render()
+
+        if not done:
+            max_future_q = np.max(q_table[new_discrete_state])
+            current_q = q_table[discrete_state + (action, )]
+
+            new_q = (1-LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+            q_table[discrete_state + (action,)] = new_q # we update q table after taking an action...
+        
+        elif new_state[0] >= env.goal_position:
+            print(f'Already success: {episode}')
+            q_table[discrete_state + (action, )] = 0
+
+        discrete_state = new_discrete_state
+
+    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
+        epsilon -= epsilon_decay_value
+
+    ep_rewards.append(episode_reward)
+
+    if not episode % SHOW_EVERY:
+        average_reward = sum(ep_rewards[-SHOW_EVERY:]) / len(ep_rewards[-SHOW_EVERY:])
+        aggr_ep_rewards['ep'].append(episode)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['min'].append(min(ep_rewards[-SHOW_EVERY:]))
+        aggr_ep_rewards['max'].append(max(ep_rewards[-SHOW_EVERY:]))
+
+        print(f'Episode: {episode}, avg: {average_reward}, min: {min(ep_rewards[-SHOW_EVERY:])}, max: {max(ep_rewards[-SHOW_EVERY:])}')
+
+env.close()
+
+# plot will start at (0, -200) as there are no rewards for
+# actions, where max_episodes_steps are 200...
+# And the reward is 0, for bad step it's -1
+# So if y==-200 it means that agent didn't make it to the cloud.
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="avg")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max")
+
+plt.legend(loc=4)
+plt.show()
